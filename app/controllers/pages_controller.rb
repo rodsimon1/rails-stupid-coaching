c540b64a9c82be3629e33eb3a69704be1c33ab3c
Rodrigo Simon <rodrigo.simon85@gmail.com>
2019-11-18 16:58:04 -0300

    foking stupid coach

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def answer
+  end
+end
---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+    # raise
+    # params[]
+  end
+
+  def answer
+    # raise
+    if params[:question] == 'I am going to work'
+      return @answer = 'Great!'
+    elsif params[:question].include? '?'
+      return @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
